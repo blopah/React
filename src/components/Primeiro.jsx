795eb102ff99ad6288a1eb50714d13b69ff5eaab
Pablo Lucena <pablodplucenahh@gmail.com>
2021-01-07 20:21:43 -0300

    css add, components add, Primeiro add

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const myVar = (props) => {
+    return <div className="Primeiro">
+        <h2>{props.titulo}</h2>
+        <h1>Primeiro</h1>
+    </div>
+};
+
+export default myVar;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './components/Primeiro'
+
 ReactDOM.render(
-    <h1>Ola React!</h1>,
+    <Primeiro></Primeiro>,
     document.getElementById('root')
 )
