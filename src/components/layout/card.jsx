defe771bf046934474394459d4a9ef738a801ad6
Pablo Lucena <pablodplucenahh@gmail.com>
2021-01-18 20:51:30 -0300

    cores otimizadas2

--- src/components/layout/card.jsx
+++ src/components/layout/card.jsx
@@ -4,11 +4,11 @@ import './card.css'
 import React from 'react'
 
 const myVar = (props) => {
-    return <div className="card" style={{backgroundColor: props.color || '#ff0000'}}>
+    return <div className="card" style={{backgroundColor: props.color || '#F7B733'}}>
         <div className="conteudo">
             {props.children}
         </div>
-        <div className="footer" style={{backgroundColor: props.color || '#ff0000'}}>
+        <div className="footer" style={{backgroundColor: props.color || '#F7B733'}}>
             {props.titulo}
         </div>
     </div>
