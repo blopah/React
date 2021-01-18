9bb298b9b91af5c024e545d48a163a4b873c296f
Pablo Lucena <pablodplucenahh@gmail.com>
2021-01-18 20:47:30 -0300

    cores adicionadas nos outros cards

--- src/components/layout/card.jsx
+++ src/components/layout/card.jsx
@@ -4,11 +4,11 @@ import './card.css'
 import React from 'react'
 
 const myVar = (props) => {
-    return <div className="card" style={{backgroundColor: props.color}}>
+    return <div className="card" style={{backgroundColor: props.color || '#ff0000'}}>
         <div className="conteudo">
             {props.children}
         </div>
-        <div className="footer" style={{backgroundColor: props.color}}>
+        <div className="footer" style={{backgroundColor: props.color || '#ff0000'}}>
             {props.titulo}
         </div>
     </div>
