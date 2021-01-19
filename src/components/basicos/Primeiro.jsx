0190f14eb1e2ec30801e31269fb9d1eca73577e6
Pablo Lucena <pablodplucenahh@gmail.com>
2021-01-19 08:20:44 -0300

    teste de alteracao em props

--- src/components/basicos/Primeiro.jsx
+++ src/components/basicos/Primeiro.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const myVar = (props) => {
     return <div className="Primeiro">
-        <h1>{props.titulo}</h1>
+        <h1>{props.titulo.toUpperCase()}</h1>
     </div>
 };
 
