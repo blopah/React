8c3c12a5522832b278052ab71ac3a51bd972fdaa
Pablo Lucena <pablodplucenahh@gmail.com>
2021-01-24 22:51:39 -0300

    getSeconds add

--- src/components/comunicacao/indireta2/Estado.jsx
+++ src/components/comunicacao/indireta2/Estado.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const myVar = (props) => {
-
+    
     function plane() {
         return props.mundo('Planeta')
     }
--- src/components/comunicacao/indireta2/Pais.jsx
+++ src/components/comunicacao/indireta2/Pais.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Estado from './Estado.jsx'
 
-const myVar = (props) => {
+const MyVar = (props) => {
+
+    var today;
+
+    function newDate() {
+        today = new Date();
+    }
+
+    setInterval(newDate, 1000)
+
+    function clikou() {
+        console.log(today.getSeconds())
+    }
 
     function world(plan) {
         return plan + ' Terra';
     }
 
     return <div>
-        <Estado mundo={world}>Hello </Estado>
+        <Estado mundo={world}>Hello</Estado>
+        <button onClick={clikou}>getSeconds</button>
     </div>
 };
 
-export default myVar;
+export default MyVar;
