075f9441a5800684904447db64f4edc7b7f84e7c
blopah <pablodplucenahh@gmail.com>
2021-01-31 23:38:06 -0300

    pratica em comunicacao indireta com set interval

--- src/components/comunicacao/indireta2/Estado.jsx
+++ src/components/comunicacao/indireta2/Estado.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 const YourVar = (props) => {
+
+    // variavel que vai armazenar a data
+    // var todays;
+    // funcao que vai inserir a data na variavel
+    // function newDate() {
+    //     todays = new Date();
+    // }
+    // newDate()
+    // funcao que renova a data a cada 1 segundo
+    // setInterval(newDate, 1000)
+    // funcao que joga a data no console
     
     function plane() {
         return props.mundo('Planeta')
--- src/components/comunicacao/indireta2/Pais.jsx
+++ src/components/comunicacao/indireta2/Pais.jsx
@@ -2,26 +2,19 @@ import React from 'react'
 import Estado from './Estado.jsx'
 
 const YourVar = (props) => {
-
-    var todays;
-
-    function newDate() {
-        todays = new Date();
-    }
-
-    setInterval(newDate, 1000)
-
-    function clikou() {
-        console.log(todays.getSeconds())
-    }
-
+    
+    // funcao que pega o retorno do componente filho e retorna
     function world(plan) {
         return plan + ' Terra';
     }
 
+    var [sec, setSec] = React.useState(new Date().getSeconds());
+
+    // setInterval(setSec(sec += 1), 1000)
+
     return <div>
-        <Estado mundo={world}>Hello</Estado>
-        <div className="button" onClick={clikou}>getSeconds</div>
+        <Estado mundo={world}>Ola</Estado> {/* chama o componente filho, enviando a funcao e tambem childrens */}
+        <div className="button">Second: {sec}</div> {/* botao */}
     </div>
 };
 
