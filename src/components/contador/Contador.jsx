0e662ac53cb0121c6ca9d4edcd1f9c5afc580565
blopah <pablodplucenahh@gmail.com>
2021-02-01 23:02:33 -0300

    valores de exibição add ao contador

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,11 +1,19 @@
 import React, {Component} from 'react'
 
 const myVar = class Contador extends Component {
+
+    state = {
+        passo: this.props.passo || 1,
+        valor: this.props.valor || 0,
+    }
+
     render () {
         return (
             <>
                 <h2>Contador</h2>
-                <h4>Display</h4>
+                <h4>Passo: {this.state.passo}</h4>
+                <h4>Valor: {this.state.valor}</h4>
+                
                 <div>
                     <button>+</button>
                     <button>-</button>
