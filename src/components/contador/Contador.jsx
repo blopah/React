de79bd9f4c34750ac9dc0d13c9b18a05652bd857
blopah <pablodplucenahh@gmail.com>
2021-02-02 21:31:23 -0300

    contador melhorado

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -27,7 +27,11 @@ const myVar = class Contador extends Component {
             <>
                 <h2>Contador</h2>
                 <h4>Passo: {this.state.passo}</h4>
+                <input type="number" value={this.state.passo}
+                onChange={e => this.setState({passo: +e.target.value})}/>
                 <h4>Valor: {this.state.valor}</h4>
+                <input type="number" value={this.state.valor}
+                onChange={e => this.setState({valor: +e.target.value})}/>
 
                 <div>
                     <button onClick={this.inc}>+</button>
