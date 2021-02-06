ae4a05c21bcbdb159627d1d3434dfac3162db32b
blopah <pablodplucenahh@gmail.com>
2021-02-06 20:54:45 -0300

    muita alteracao feita no css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,54 +19,60 @@ import Comunicacaoindireta2 from './comunicacao/indireta2/Pais'
 import Input from './form/Input'
 import Contador from './contador/Contador'
 import Mega from './mega/mega'
+import Info from './info/Info'
 
 const myVar = (props) => {
-    return <div className="Apps">
-        <h1>Fundamentos React</h1>
-        <div className="App">
-            <Card titulo="#12 - Mega" color="#00484">
-                <Mega qttNumbers={8}></Mega>
-            </Card>
-            <Card titulo="#11 - Contador" color="#00f484">
-                <Contador passo={10} valor={100}></Contador>
-            </Card>
-            <Card titulo="#10 - Input" color="#008484">
-                <Input></Input>
-            </Card>
-            <Card titulo="#09 - Comunicação indireta 2" color="#ff8484">
-                <Comunicacaoindireta2></Comunicacaoindireta2>
-            </Card>
-            <Card titulo="#08 - Comunicação indireta" color="#848484">
-                <Comunicacaoindireta></Comunicacaoindireta>
-            </Card>
-            <Card titulo="#07 - Comunicação direta" color="#f00000">
-                <Comunicacaodireta sobrenome="Freitas"></Comunicacaodireta>
-            </Card>
-            <Card titulo="#06 - Condicional Com If" color="#00f000">
-                <CondicionalComIf numero={8}></CondicionalComIf>
-            </Card>
-            <Card titulo="#05 - Condicional" color="#00fff0">
-                <Condicional numero={9}></Condicional>
-            </Card>
-            <Card titulo="#04 - Repetição" color="#000f00">
-                <Repeticao></Repeticao>
-            </Card>
-            <Card titulo="#03 - Com Filhos" color="#0000f0">
-                <ComFilhos>
-                    <ul>
-                        <li>Python</li>
-                        <li>Java Script</li>
-                        <li>Java</li>
-                        <li>PHP</li>
-                    </ul>
-                </ComFilhos>
-            </Card>
-            <Card titulo="#02 - Com Parametros" color="#f000f0">
-                <ComParametro pai="Pablo" mae="Vitória" filho="Joaquim"></ComParametro>
-            </Card>
-            <Card titulo="#01 - Primeiro">
-                <Primeiro titulo="Primeiro"></Primeiro>
-            </Card>
+    return <div className="App">
+        <div className="info">
+            <Info></Info>
+        </div>
+        <div className="items">
+            <h1>Fundamentos React</h1>
+            <div className="cards">
+                <Card titulo="#12 - Mega" color="#00484">
+                    <Mega qttNumbers={8}></Mega>
+                </Card>
+                <Card titulo="#11 - Contador" color="#00f484">
+                    <Contador passo={10} valor={100}></Contador>
+                </Card>
+                <Card titulo="#10 - Input" color="#008484">
+                    <Input></Input>
+                </Card>
+                <Card titulo="#09 - Comunicação indireta 2" color="#ff8484">
+                    <Comunicacaoindireta2></Comunicacaoindireta2>
+                </Card>
+                <Card titulo="#08 - Comunicação indireta" color="#848484">
+                    <Comunicacaoindireta></Comunicacaoindireta>
+                </Card>
+                <Card titulo="#07 - Comunicação direta" color="#f00000">
+                    <Comunicacaodireta sobrenome="Freitas"></Comunicacaodireta>
+                </Card>
+                <Card titulo="#06 - Condicional Com If" color="#00f000">
+                    <CondicionalComIf numero={8}></CondicionalComIf>
+                </Card>
+                <Card titulo="#05 - Condicional" color="#00fff0">
+                    <Condicional numero={9}></Condicional>
+                </Card>
+                <Card titulo="#04 - Repetição" color="#000f00">
+                    <Repeticao></Repeticao>
+                </Card>
+                <Card titulo="#03 - Com Filhos" color="#0000f0">
+                    <ComFilhos>
+                        <ul>
+                            <li>Python</li>
+                            <li>Java Script</li>
+                            <li>Java</li>
+                            <li>PHP</li>
+                        </ul>
+                    </ComFilhos>
+                </Card>
+                <Card titulo="#02 - Com Parametros" color="#f000f0">
+                    <ComParametro pai="Pablo" mae="Vitória" filho="Joaquim"></ComParametro>
+                </Card>
+                <Card titulo="#01 - Primeiro">
+                    <Primeiro titulo="Primeiro"></Primeiro>
+                </Card>
+            </div>
         </div>
     </div>
 };
---
+++ src/components/info/Info.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './info.css'
+
+const myVar = (props) => {
+    return (
+        <div className="info">
+            <div className="perfil">
+                <img width="100%" height="100%" src="https://imagens.netlify.app/perfil.png" alt=""/>
+            </div>
+            <h1 className="nome">Pablo Lucena</h1>
+            <p className="email">pablodplucenahh@gmail.com</p>
+            <div className="redes">
+                <div className="lkd">
+                    <img width="100%" height="100%" src="https://imagens.netlify.app/lkd.svg" alt=""/>
+                </div>
+                <div className="git">
+                    <img width="100%" height="100%" src="https://imagens.netlify.app/git.svg" alt=""/>
+                </div>
+            </div>
+            <div className="linha"></div>
+            <h3 className="desc">
+            Esta página foi criada para praticar os fundamentos em
+            </h3>
+            <div className="em-react">
+                <h1>React</h1>
+                <div className="react-logo1">
+                    <img width="100%" height="100%" src="https://imagens.netlify.app/reactreact-logo.svg" alt=""/>
+                </div>
+            </div>
+                <div className="chapa">
+                    <p className="agrad">
+                    Cada item desta página, representa um conceito de React que foi aprendido e praticado.
+<br></br>
+<br></br>
+Deixo meus sinceros agradecimentos ao professor Leonardo Leitão e também à escola Cod3r, que disponibilizaram o conhecimento de forma gratuita no youtube.
+                    </p>
+                    <h2 className="obrigado">
+                        Muito Obrigado!
+                    </h2>
+                </div>
+        </div>
+    )
+}
+
+export default myVar;
