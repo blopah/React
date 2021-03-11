e919c03b0c97ebeecde3337db7f8ee8935a4e260
Pablo Lucena <pablodplucenahh@gmail.com>
2021-03-11 13:28:24 -0300

    input do if em andamento

--- src/components/basicos/CondicionalComIf.jsx
+++ src/components/basicos/CondicionalComIf.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from 'react'
 import If from "./If";
 
-const myVar = props => {
+const MyVar = props => {
+
+    const [name, setName] = useState('')
+
     return <div>
-        <h2>O número {props.numero} é</h2>
-        <If test={props.numero % 2 === 0}>
-            <span>Par</span>
+        <h2>O número</h2>
+        <input placeholder="Insira um número" type="number" className="input dez" value={name} 
+            onChange={e => setName(e.target.value)}
+            />
+        <h2>é</h2>
+        <If test={name % 2 === 0}>
+            <h2>Par</h2>
         </If>
-        <If test={props.numero % 2 === 1}>
-            <span>Impar</span>
+        <If test={name % 2 === 1}>
+            <h2>Impar</h2>
         </If>
+            
     </div>
 }
 
-export default myVar;
+export default MyVar;
--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -5,39 +5,43 @@ const myVar = (props) => {
     return (
         <div className="info">
             <div className="perfil">
-                <img width="100%" height="100%" src="https://imagens.netlify.app/perfil.png" alt=""/>
+                <img width="100%" height="100%" src="https://imagens.netlify.app/perfil.png" alt="" />
             </div>
             <h1 className="nome">Pablo Lucena</h1>
             <p className="email">pablodplucenahh@gmail.com</p>
             <div className="redes">
-                <div className="lkd">
-                    <img width="80%" height="100%" src="https://imagens.netlify.app/lkd.svg" alt=""/>
-                </div>
-                <div className="git">
-                    <img width="80%" height="100%" src="https://imagens.netlify.app/git.svg" alt=""/>
-                </div>
+                <a href="https://www.linkedin.com/in/pablo-lucena/" target="_blank" rel="noopener noreferrer">
+                    <div className="lkd">
+                        <img width="80%" height="100%" src="https://imagens.netlify.app/lkd.svg" alt="" />
+                    </div>
+                </a>
+                <a href="https://github.com/blopah/Curriculo" target="_blank" rel="noopener noreferrer">
+                    <div className="git">
+                        <img width="80%" height="100%" src="https://imagens.netlify.app/git.svg" alt="" />
+                    </div>
+                </a>
             </div>
             <div className="linha"></div>
             <h3 className="desc">
-            Esta página foi criada para praticar os fundamentos em
+                Esta página foi criada para praticar os fundamentos em
             </h3>
             <div className="em-react">
                 <h1>React</h1>
                 <div className="react-logo1">
-                    <img width="100%" height="100%" src="https://imagens.netlify.app/reactreact-logo.svg" alt=""/>
+                    <img width="100%" height="100%" src="https://imagens.netlify.app/reactreact-logo.svg" alt="" />
                 </div>
             </div>
-                <div className="chapa">
-                    <p className="agrad">
+            <div className="chapa">
+                <p className="agrad">
                     Cada item desta página, representa um conceito de React que foi aprendido e praticado.
 <br></br>
-<br></br>
+                    <br></br>
 Deixo meus sinceros agradecimentos ao professor Leonardo Leitão e também à escola Cod3r, que disponibilizaram o conhecimento de forma gratuita no youtube.
                     </p>
-                    <h2 className="obrigado">
-                        Muito Obrigado!
+                <h2 className="obrigado">
+                    Muito Obrigado!
                     </h2>
-                </div>
+            </div>
         </div>
     )
 }
